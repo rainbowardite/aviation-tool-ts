003e68049ec7611bc69cfc57ec56a4865eac0e0e
Lexi <nblexi98@gmail.com>
2026-08-14 02:53:11 -0500

    fixing pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: '/aviation-tool-ts/',
   build: {
       outDir: 'docs',
       emptyOutDir: true, // Clears the folder before building
