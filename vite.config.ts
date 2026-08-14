0aa66970cf396beb222207300c2977a97f1872a9
Lexi <nblexi98@gmail.com>
2026-08-14 03:03:55 -0500

    trying full link

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/aviation-tool-ts/',
+  base: 'https://rainbowardite.github.io/aviation-tool-ts/',
   build: {
       outDir: 'docs',
       emptyOutDir: true,
